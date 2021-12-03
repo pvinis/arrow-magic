7ee613ca3a1e2cda3a7ad0a195e6c6c1394e185d
Pavlos Vinieratos <pvinis@gmail.com>
2021-12-03 10:55:12 -0500

    batman

---
+++ src/magic.ts
@@ -0,0 +1,12 @@
+const example = [{ href: 'aha1' }, { href: 'aha2' }]
+
+const $ =
+	<Ks extends string, Rec extends Record<Ks, any>>(attribute: Ks) =>
+	(item: Rec) =>
+		item && item[attribute]
+
+const hrefs = example.map($('href'))
+console.log(hrefs)
+
+////tests, add more symbols to export as, maybe `prop` too
+///try to use like $`href`
